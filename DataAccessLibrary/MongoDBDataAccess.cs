7db8e6f10277ed0a4ce2f48add61fbb01df559b6
Corbyn Altman <corbyn.altman01@gmail.com>
2022-10-10 19:27:36 -0400

    Add project files.

---
+++ DataAccessLibrary/MongoDBDataAccess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DataAccessLibrary;
+
+public class MongoDBDataAccess
+{
+	private readonly IMongoDatabase _db;
+	public MongoDBDataAccess(string dbName, string connectionString)
+	{
+		var client = new MongoClient(connectionString);
+		_db = client.GetDatabase(dbName);
+	}
+
+	public void InsertRecord<T>(string table, T record)
+	{
+		var collection = _db.GetCollection<T>(table);
+		collection.InsertOne(record);
+	}
+
+	public List<T> LoadRecords<T>(string table)
+	{
+		var collection = _db.GetCollection<T>(table);
+		return collection.Find(new BsonDocument()).ToList();
+	}
+
+	public T LoadRecordById<T>(string table, Guid id)
+	{
+		var collection = _db.GetCollection<T>(table);
+		var filter = Builders<T>.Filter.Eq("Id", id);
+
+		return collection.Find(filter).First();
+	}
+
+	public void UpsertRecord<T>(string table, Guid id, T record)
+	{
+		var collection = _db.GetCollection<T>(table);
+
+		_ = collection.ReplaceOne(
+			new BsonDocument("_id", new BsonBinaryData(id, GuidRepresentation.Standard)),
+			record,
+			new ReplaceOptions { IsUpsert = true });
+	}
+
+	public void DeleteRecord<T>(string table, Guid id)
+	{
+		var collection = _db.GetCollection<T>(table);
+		var filter = Builders<T>.Filter.Eq("Id", id);
+		collection.DeleteOne(filter);
+	}
+}
+
---
+++ MongoDBUI/Program.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+
+MongoDBDataAccess db = new("demo", GetConnectionString());
+const string tableName = "Contacts";
+
+ContactModel newPerson = new()
+{
+    FirstName = "Corbyn",
+    LastName = "Altman",
+};
+
+newPerson.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "corbyn@corbynaltman.com" });
+newPerson.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "2603416708" });
+
+CreateContact(newPerson);
+GetAllContacts();
+
+
+Console.WriteLine("Done Processing Mongo");
+
+Console.ReadLine();
+
+void GetAllContacts()
+{
+    var contacts = db.LoadRecords<ContactModel>(tableName);
+
+    contacts.ForEach(c =>
+    {
+        Console.WriteLine($"{c.FirstName} {c.LastName}");
+    });
+}
+
+void CreateContact(ContactModel contact, string table = tableName)
+{
+    db.UpsertRecord(table, contact.Id, contact);
+    Console.WriteLine($"Contact Created: {contact.FirstName} {contact.LastName}");
+}
+
+static string GetConnectionString(string connectionStringName = "Default")
+{
+    string output = "";
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionStringName);
+
+    return output;
+}
